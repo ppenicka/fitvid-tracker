81917965be7302f7ece5ad675958a87050af1a55
Petr Penicka <petr.penicka@gmail.com>
2020-05-21 15:31:30 +0200

    feat: Koa serving built static client assets

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
+const serve = require("koa-static");
+const mount = require("koa-mount");
 const mongoose = require('mongoose');
 
 const router = require('./routes/router.js');
 
 const app = new Koa();
+const static_pages = new Koa();
+static_pages.use(serve(__dirname + '/../../client/build'));
+app.use(mount("/", static_pages));
+
 
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => {
   if (error) {
@@ -15,7 +21,6 @@ mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopo
     console.log('connnected to db');
   }
 } );
-
 
 app
   .use(cors())
