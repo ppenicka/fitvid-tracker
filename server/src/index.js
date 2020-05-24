d3a716f1cc6b3b74ab6ad61703cb838a9dd9ff54
Petr Penicka <petr.penicka@gmail.com>
2020-05-24 15:45:50 +0200

    fix: correct serving of built client

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 const serve = require("koa-static");
+const send = require("koa-send");
 const mount = require("koa-mount");
 const mongoose = require('mongoose');
 
@@ -10,10 +11,7 @@ const router = require('./routes/router.js');
 
 const app = new Koa();
 const static_pages = new Koa();
-// static_pages.use(serve(__dirname + '/../../client/build'));
-// app.use(mount("/", static_pages));
-app.use(serve(__dirname + '/../../client/build'));
-
+static_pages.use(serve(__dirname + '/../../client/build'));
 
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => {
   if (error) {
@@ -27,7 +25,15 @@ app
   .use(cors())
   .use(bodyParser())
   .use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
+  .use(mount("/", static_pages))
+  .use(async (ctx) => {
+    await send(ctx, 'index.html', {root: __dirname + '/../../client/build/'})
+  });
+
+  // .use(function* index() {
+  //   yield send(this, 'index.html', {root: '../client/build/'});
+  // });
 
 app.listen(process.env.PORT);
 console.log(`Server listening on ${process.env.SERVER_HOST}:${process.env.PORT}`);
